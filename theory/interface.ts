7b0b1466445d1b0862de5a1977171555b6d4a695
mindong2 <qlsnaos12@naver.com>
2023-04-05 23:37:16 +0900

    interface,type  Recap & interface + generic on class property

--- theory/interface.ts
+++ theory/interface.ts
@@ -84,3 +84,92 @@ class Player implements User2, Human {
     return `hello ${name} ! my name is ${this.fullname()}`;
   }
 }
+
+// recap
+
+// type
+type PlayerA = {
+  name: string;
+};
+
+// type 상속시
+
+type PlayerB = PlayerA & {
+  nickname: string;
+};
+
+const TypePlayer: PlayerB = {
+  name: "dongmin",
+  nickname: "mindong",
+};
+
+// interface
+
+interface Player1 {
+  name: string;
+}
+
+// interface 상속시
+
+interface Player2 extends Player1 {
+  nickname: string;
+}
+
+//interface는 재선언하므로써 새로운 속성추가가 가능하다 type은 오류가 남 (차이점)
+interface Player2 {
+  age: number;
+}
+
+class PlayerClass implements Player2 {
+  constructor(
+    public name: "dongmin",
+    public nickname: "mindong",
+    public age: 2
+  ) {}
+}
+
+// type 또한 implements가 가능하다 (공통점)
+
+class PlayerClass2 implements PlayerB {
+  constructor(public name: "dongmin", public nickname: "mindong") {}
+}
+
+/*
+  정리
+
+  1. type과 interface 모두 implements 가능하다
+  2. type의 상속방법은 &을통해, interface는 extends를 통해
+  3. type은 재선언이 안되며 interface는 재선언을통해 속성추가가 가능하다
+  4. Object 타입은 interface를 사용하자
+*/
+
+// Generic 추가
+
+interface MyStorage<T> {
+  [key: string]: T;
+}
+
+class LocalStorage<T> {
+  //해당 클래스에서 interface로 generic 함수 전달
+  private storage: MyStorage<T> = {};
+  set(key: string, value: T) {
+    this.storage[key] = value;
+  }
+  remove(key: string) {
+    delete this.storage[key];
+  }
+  get(key: string): T {
+    return this.storage[key];
+  }
+  clear() {
+    this.storage = {};
+  }
+}
+
+const stringStorage = new LocalStorage<string>();
+stringStorage.set("name", "dongmin");
+stringStorage.get("name");
+
+const booleanStorage = new LocalStorage<boolean>();
+booleanStorage.get("name");
+booleanStorage.set("merried", false);
